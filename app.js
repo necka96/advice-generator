d856ab1df2a96baa37d8f79821a213b3f144dd02
necka96 <nemanja2014@live.com>
2022-02-25 02:34:34 +0100

    animate2

--- app.js
+++ app.js
@@ -9,11 +9,29 @@ $(".btn").click(()=>{
   url: "https://api.adviceslip.com/advice",
   dataType: "json",
   success: function (response) {
-   $(".id").html(response.slip.id)
+   $(".id").animate({letterSpacing: "+=10px", opacity: 0}, 400,function(){
+       $(this).html("Advice #" + response.slip.id ).animate({letterSpacing: "-=10px", opacity: 1}, 400)
+   })
    $(".advice").animate({'opacity': 0}, 400, function(){
-        $(this).html(response.slip.advice).animate({'opacity': 1}, 400);    
+        $(this).html('"'+ response.slip.advice + '"').animate({'opacity': 1}, 400);    
     });
    $(".btn").removeClass("active")
   }
  });
 })
+$(document).ready(function () {
+   
+ $.ajax({
+  type: "GET",
+  url: "https://api.adviceslip.com/advice",
+  dataType: "json",
+  success: function (response) {
+   $(".id").animate({letterSpacing: "+=10px", opacity: 0}, 400,function(){
+       $(this).html("Advice #" + response.slip.id ).animate({letterSpacing: "-=10px", opacity: 1}, 400)
+   })
+   $(".advice").animate({'opacity': 0}, 400, function(){
+        $(this).html('"'+ response.slip.advice + '"').animate({'opacity': 1}, 400);    
+    });
+  }
+ });
+});
